5a22831f4726191efb3e26a125ae8af52d307677
Sergey Padzhev <serhio_j@mail.ru>
2016-10-27 21:46:55 +0300

    Описали элементы калькулятора.

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Calculator {
+	
+	// Объявляем элементы калькулятора.
+	JPanel windowContent;
+	JTextField displayField
+}
